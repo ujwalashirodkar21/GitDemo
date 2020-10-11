052afed72f8c5493a621716d1f0c6c832cfe370e
ujwala.shirodkar21@gmail.com <ujwala.shirodkar21@gmail.com>
2020-10-11 19:49:18 +0530

    This a commit from Asian time zone

--- src/test/java/mavengroup/mavenartifact/APITest.java
+++ src/test/java/mavengroup/mavenartifact/APITest.java
@@ -6,7 +6,9 @@ public class APITest {
 	@Test
 	public void postJIRA()
 	{
-		System.out.println("postJIRA");
+		System.out.println("postJIRA1");
+		System.out.println("postJIRA2");
+		System.out.println("postJIRA3");
 	}
 	
 	@Test
